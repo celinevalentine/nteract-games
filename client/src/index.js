75b8d3b5746fc792950667f308a1db3986589a7e
Celine Valentine <cyv419@gmail.com>
2021-02-26 11:39:51 -0800

    modified index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,17 +5,14 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App";
 import { GameProvider } from "./components/context/GameContext";
 
-// const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-// const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-//
-// dev-pwb5v-aq.us.auth0.com
-// FUkZCqf5UcsHYWOZ0Yl3QXFii466O1Mc
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev-pwb5v-aq.us.auth0.com"
-      clientId="FUkZCqf5UcsHYWOZ0Yl3QXFii466O1Mc"
+      domain={domain}
+      clientId={clientId}
       redirectUri={window.location.origin}>
       <GameProvider>
         <App />
