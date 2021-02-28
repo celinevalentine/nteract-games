3760239e618e6b2605db18c5d24667ed08f48d0e
Celine Valentine <cyv419@gmail.com>
2021-02-27 23:59:51 -0800

    changed root directory

---
+++ server/node_modules/@auth0/auth0-spa-js/dist/typings/scope.d.ts
@@ -0,0 +1,8 @@
+/**
+ * @ignore
+ */
+export declare const getUniqueScopes: (...scopes: string[]) => string;
+/**
+ * @ignore
+ */
+export declare const getMissingScope: (originalScope?: string, comparingScope?: string) => string;
