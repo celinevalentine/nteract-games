0ae9a12cd4fbe5da3b9a9bc6ad45c448604f2410
Celine Valentine <cyv419@gmail.com>
2021-02-27 22:01:44 -0800

    modified backend package json

--- server/app.js
+++ server/app.js
@@ -5,11 +5,20 @@ const path = require("path");
 
 const ExpressError = require("./expressError");
 const app = express();
-const pool = require("./db");
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+const ENV = process.env.NODE_ENV;
+const PORT = process.env.PORT || 5000;
+
+if (ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+}
 
 // routes
 
