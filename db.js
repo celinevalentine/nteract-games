35d201094bcec3d5e0b2eb783f3d168370342917
Celine V <dandan@celines-imac.home>
2021-03-24 23:38:33 -0700

    modified ssl logic

--- db.js
+++ db.js
@@ -3,9 +3,11 @@ const { DB_URI } = require("./config");
 
 const db = new Client({
   connectionString: DB_URI,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  ssl: process.env.DATABASE_URL
+    ? {
+        rejectUnauthorized: false,
+      }
+    : false,
 });
 
 db.connect();
